15f82ec1731a0d7fb1d81134300e0649016413c8
Abrar Khawar <akabrar5@gmail.com>
2026-01-29 01:10:22 +0530

    connected to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./utils/db";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
+connectDb();
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
 });
